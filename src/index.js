addb7a36562091c8cb6ba4aa2c7664130ae86efd
Artem <artem-git@clinicbuddy.com>
2020-02-27 13:32:15 +0300

    feat: add solved Towel Sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  var arr = [], buf = [];
+  if (matrix != undefined) {
+    for (var i = 0; i < matrix.length; i++) {
+      if (i % 2 == 0) {
+        buf = matrix[i];
+      } else {
+        buf = matrix[i].reverse();
+      }
+
+      for (var j = 0; j < buf.length; j++) {
+        arr.push(buf[j]);
+      }
+    }
+  }
+  return arr;
 }
